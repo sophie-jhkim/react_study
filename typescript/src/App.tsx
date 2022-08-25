83ea45de24c4c484148544cb325db2d28bbe749c
nextinnovation-wisekim <jihye-kim@nextinnovation.kr>
2022-08-25 18:10:55 +0900

    arrow function 변경 및 useState 사용

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import './assets/css/style.css'
+import InputField from './components/InputField';
+import { Todo } from './model';
 
-function App() {
+const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  // const [todo, setTodo] = useState(""); 괄호안에 ""만 넣어도 타입은 string으로 인식하지만 명확히 하기 위해 <string>을 써준다
+  const [todos, useTodos] = useState<Todo[]>([]);
+
+  const addTodo = (event: React.FormEvent) =>{
+    event.preventDefault();
+    if(todo){
+      //TODO  https://youtu.be/FJDVKeh7RJI?t=2120 여기부터
+    }
+
+  }
   return (
     <div className="App">
-      Hello world
+      <h1 className="heading">To Do List</h1>
+      <InputField todo={todo} setTodo={setTodo} addTodo={addTodo}/>
     </div>
   );
 }
