982057109258381c01f72f4390574919f103663d
nextinnovation-wisekim <jihye-kim@nextinnovation.kr>
2022-08-26 15:40:26 +0900

    reducer 참고 코드

--- typescript/src/model.ts
+++ typescript/src/model.ts
@@ -4,3 +4,30 @@ export interface Todo{
   isDone: boolean;
 }
 
+/* reducer
+type Actions={ type:'add', payload:string} | { type:'remove', payload:number} |{ type:'done', payload:number} ;
+
+const TodoReducer = (state:Todo[], action: Actions) =>{
+  switch(action.type){
+    case "add":
+      return [ ...state, {id : Date.now(), todo: action.payload, isDone: false}] ;
+    case "remove":
+      return state.filter((todo) => todo.id !== action.payload);
+    case "done":
+      return state;
+    default:
+      return state;
+  }
+
+}
+
+import { useReducer } from "react";
+const ReducerExample = () =>{
+
+  const [state, dispatch] = useReducer(TodoReducer, [])
+
+  return (
+    <div/>
+  )
+}
+*/
