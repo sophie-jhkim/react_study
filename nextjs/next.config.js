89d0eb824fe72ecaa08e28ae5fb0bb06f3a19416
nextinnovation-wisekim <jihye-kim@nextinnovation.kr>
2022-08-29 12:51:50 +0900

    next.config.js redirect, rewrite

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [{ source: "/contact", destination: "/form", permanent: false }];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: ``,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
