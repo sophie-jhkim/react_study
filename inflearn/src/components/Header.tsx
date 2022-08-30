871598408dc7001a2d519d76b956713cf355f301
nextinnovation-wisekim <jihye-kim@nextinnovation.kr>
2022-08-30 17:01:20 +0900

    layout 컴포넌트 생성

---
+++ inflearn/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <div style={{ border: 2, padding: 2, borderColor: 'black', borderStyle: 'dashed', margin: 5, width: 500, height: 500 }}>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Header;
