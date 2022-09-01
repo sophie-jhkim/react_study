a180c76d1b9e12f4ccc6365bae96098e02e3f0da
nextinnovation-wisekim <jihye-kim@nextinnovation.kr>
2022-09-01 19:11:17 +0900

    고양이/강아지/그외 카테고리

---
+++ nextjs/pages/dogs.js
@@ -0,0 +1,22 @@
+import Seo from './Seo';
+import BaseCard from '../components/BaseCard';
+
+export default function Dogs({ response }) {
+  return (
+    <div className="container">
+      <Seo title="Home"></Seo>
+      {response.body.items.item?.map((animal) => (
+        <BaseCard animal={animal} key={animal.desertionNo} />
+      ))}
+    </div>
+  );
+}
+
+export async function getServerSideProps() {
+  const { response } = await (await fetch(`${process.env.NEXT_PUBLIC_API_URL}?serviceKey=${process.env.SERVICE_KEY}&upkind=417000&_type=json`)).json();
+  return {
+    props: {
+      response
+    }
+  };
+}
